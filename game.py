57e4e01486f8a6dde2949245bfe7bcf6d2b43381
Anshul Shirude <34725764+AnshulShirude@users.noreply.github.com>
2022-02-27 13:15:57 -0500

    DGetting the program to see the nose on the screen

--- game.py
+++ game.py
@@ -1,14 +1,27 @@
 import cv2
+import sys
+cascPath = sys.argv[1]
+noseCascade = cv2.CascadeClassifier(cascPath)
 
-camera = cv2.VideoCapture(0)
+video_capture = cv2.VideoCapture(0)
 
 while True:
-    success, frame = camera.read()
-    
-    cv2.imshow("Recording Window", frame)
-    
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    nose = noseCascade.detectMultiScale(gray, 1.3, 5)
+    # Draw a rectangle around the nose
+    for (x, y, w, h) in nose:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-camera.release()
+# When everything is done, release the capture
+video_capture.release()
 cv2.destroyAllWindows()
